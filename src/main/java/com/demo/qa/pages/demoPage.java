4ee09c8068c7bcc43d85e669685f1aaa3f7cf4a7
CORPORATE_NT\Kaur, Navneet <navneet.kaur@kronos.com>
2019-02-04 08:07:26 +0530

    first commit

---
+++ src/main/java/com/demo/qa/pages/demoPage.java
@@ -0,0 +1,13 @@
+package com.demo.qa.pages;
+
+public class demoPage {
+  
+	public void tes()
+	{
+		System.out.println("demo test !!");
+	}
+	public void HomePage()
+	{
+		System.out.println("HomePage");
+	}
+}
---
+++ src/test/java/com/qa/demo/testcases/Login.java
@@ -0,0 +1,13 @@
+package com.qa.demo.testcases;
+
+import org.testng.annotations.Test;
+
+public class Login {
+
+@Test
+public void Login()
+{
+	System.out.println("Login");
+}
+ 
+}
